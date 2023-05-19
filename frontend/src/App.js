ee080c01c140cf9df101e89a8bb2edef21d66f17
Jay W <977482173@qq.com>
2023-05-19 10:08:57 +0800

    5.19

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,43 @@
-import {useCallback, useLayoutEffect, useReducer, useState} from "react";
+import React from "react";
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+import {Icon} from "leaflet";
+// import * as parkData from "./data/skateboard-parks.json";
+import "./App.css";
+
+
+export const icon = new Icon({
+    iconUrl: "",
+    iconSize: [25, 25]
+});
+
+const position = [51.5, 0.0];
 
 function App() {
+    const [track, setTrack] = React.useState(null);
     return (
-        <div>
-
-        </div>
+        <MapContainer center={position} zoom={12} scrollWheelZoom={false}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={position}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
+        </MapContainer>
     );
 }
 
 export default App;
+
+
+
+
+
+
+
+
 
 
 
